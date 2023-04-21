baed0872f84cdf5a71e7e44b09ef3cac62adfe06
lucas <lucas.czeck@inteliger.com.br>
2023-04-20 22:30:45 -0300

    tentativa de gerar o crontab, verificar como funciona no windows

--- base/settings.py
+++ base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_crontab',
 
     'core',
     'core.integracoes',
@@ -132,3 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CRONJOBS = [('*/15 * * * *', 'core.integracoes.github.views.Repositories.post'),]
+
--- base/urls.py
+++ base/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+# from django_crontab import crontab
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# crontab.add_default_jobs()
