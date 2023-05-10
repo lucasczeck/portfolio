7a36bbdb844ed45e9d54fd8532b9f7480dc7bef1
lucas <lucas.czeck@inteliger.com.br>
2023-05-10 09:39:27 -0300

    Correção de erro cors

--- base/settings.py
+++ base/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200', 'https://lucasczeck.netlify.app']
+
 
 # Application definition
 
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'base.urls'
