5baf94ecad98e23f4c2ce7c526fa6a6c81c90e75
lucas <lucas.czeck@inteliger.com.br>
2023-06-10 21:39:43 -0300

    Correção na estrutura das informações da pagina about me

--- BO/aboutme/Personal.py
+++ BO/aboutme/Personal.py
@@ -4,25 +4,17 @@ import aboutme.models
 class PersonalInfos:
 
     def __init__(self):
-        self.who_am_i = self.get_who_am_i()
-        self.hobbies = self.get_hobbies()
+        self.who_am_i = self.__get_who_am_i()
 
     @staticmethod
-    def get_who_am_i():
-        who_am_i = aboutme.models.PersonalInfos.objects.active().values_list('how_i_am', flat=True).first()
+    def __get_who_am_i():
+        who_am_i = aboutme.models.Infos.objects.active().values_list('who_am_i', flat=True).first()
 
         return who_am_i
-
-    @staticmethod
-    def get_hobbies():
-        hobbies = list(aboutme.models.Hobbies.objects.active().values('descriptive_name'))
-
-        return hobbies
 
     def get_personal_infos(self):
         personal_infos = {
             'who_am_i': self.who_am_i,
-            'hobbies': self.hobbies
         }
 
         return personal_infos
--- aboutme/models.py
+++ aboutme/models.py
@@ -3,20 +3,9 @@ from django.db import models
 import core.models
 
 
-class PersonalInfos(core.models.Log):
+class Infos(core.models.Log):
     who_am_i = models.TextField(null=True)
-
-
-class Hobbies(core.models.Log):
-    descriptive_name = models.CharField(max_length=200, null=True)
-
-
-class ProfessionalInfos(core.models.Log):
-    career_summary = models.TextField(null=True)
-
-
-class Stacks(core.models.Log):
-    descriptive_name = models.CharField(max_length=200, null=True)
+    career = models.TextField(null=True)
 
 
 class Highlights(core.models.Log):
