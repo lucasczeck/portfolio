a10a33e494710bcc7998367d255f9c50f9d11653
lucas <lucas.czeck@inteliger.com.br>
2023-06-12 07:51:40 -0300

    Alteração na estrutura das infos do aboutme

--- BO/aboutme/Personal.py
+++ BO/aboutme/Personal.py
@@ -4,17 +4,29 @@ import aboutme.models
 class PersonalInfos:
 
     def __init__(self):
-        self.who_am_i = self.__get_who_am_i()
+        self.infos = None
+        self.who_am_i = None
+        self.career = None
+        self.__load()
 
-    @staticmethod
-    def __get_who_am_i():
-        who_am_i = aboutme.models.Infos.objects.active().values_list('who_am_i', flat=True).first()
+    def __get_infos(self):
+        self.infos = aboutme.models.Infos.objects.active().first()
 
-        return who_am_i
+    def __get_who_am_i(self):
+        self.who_am_i = self.infos.who_am_i
+
+    def __get_career(self):
+        self.career = self.infos.career
+
+    def __load(self):
+        self.__get_infos()
+        self.__get_career()
+        self.__get_who_am_i()
 
     def get_personal_infos(self):
         personal_infos = {
             'who_am_i': self.who_am_i,
+            'career': self.career
         }
 
         return personal_infos
